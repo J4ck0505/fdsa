826338eece91ce3a8e9c3ab2b333e277d1c8dc94
J4ck0505 <80529490+J4ck0505@users.noreply.github.com>
2021-03-13 14:48:40 +0900

    Update clean.py

--- clean.py
+++ clean.py
@@ -16,5 +16,20 @@ async def on_ready():
   #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
   
   print("Aigis:",client.user.name,"819929289759653928:",client.user.id)
+ 
+async def on_message(self, message):
+        # SENDER가 BOT일 경우 반응을 하지 않도록 한다.
+        if message.author.bot:
+            return None
+        
+        # message.content = message의 내용
+        if message.content == "aigis":
+            # 현재 채널을 받아옴
+            channel = message.channel
+            # 답변 내용 구성
+            msg = "aigis_ready"
+            # msg에 지정된 내용대로 메시지를 전송
+            await channel.send(msg)
+            return None
   
 client.run(os.environ['token'])
