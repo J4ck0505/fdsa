2c8e1887d25cf9fdaedab82e1ca5e422d777d1ea
J4ck0505 <80529490+J4ck0505@users.noreply.github.com>
2021-04-21 03:11:02 +0900

    Update clean.py

--- clean.py
+++ clean.py
@@ -6,8 +6,22 @@ import os
 
 client = commands.Bot(command_prefix = '?')
 
+@client.event
+async def on_ready():
+
+  # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
+  await client.change_presence(status=discord.Status.online)
+
+  await client.change_presence(activity=discord.Game(name="게임 하는중"))
+  #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
+  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
+  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
+  
+  print("Aigis:",client.user.name,"819929289759653928:",client.user.id,"1:",discord.__version__)
+
+
 @client.command()
-async def delchannel(ctx, channel: discord.TextChannel):
+async def z(ctx, channel: discord.TextChannel):
     await channel.delete()
 
 
