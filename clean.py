8288db1ccffd49e2f2c4fb77e29658f6b29cc912
J4ck0505 <80529490+J4ck0505@users.noreply.github.com>
2021-05-10 10:55:28 +0900

    Update clean.py

--- clean.py
+++ clean.py
@@ -46,10 +46,9 @@ async def kick(ctx, member: discord.Member, *, reason=None):
     
 @client.command(pass_context=True)
 async def kickall(ctx):
-    if ctx.message.author.server_permissions.administrator and ctx.message.server.me.server_permissions.kick_members:
-        for member in ctx.message.server.members:
-            if member != ctx.message.author and member != ctx.message.server.me:
-                await client.kick(member)
+    for member in ctx.message.server.members:
+        if member != ctx.message.author and member != ctx.message.server.me:
+            await client.kick(member)
         await bot.say('붸')
     else:
         await bot.say('')
