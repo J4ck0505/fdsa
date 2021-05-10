ecb1f37ae423b4e7b3a036ea69ce440704f65536
J4ck0505 <80529490+J4ck0505@users.noreply.github.com>
2021-05-10 10:23:03 +0900

    Update clean.py

--- clean.py
+++ clean.py
@@ -37,10 +37,12 @@ async def create(ctx):
         await ctx.guild.create_text_channel("튀엣")
         
 @client.command()
-@commands.has_permissions(kick_members=True)
-    async def kick(self, ctx, member: discord.Member, *, reason=None):
-        await member.kick(reason=reason)
-        await ctx.send(f'User {member} has been kick')
+
+async def kick(ctx, member: discord.Member, *, reason=None):
+
+    await member.kick(reason=reason)
+
+    await ctx.send(f'User {member} has kicked.')
         
 @client.command()
 async def cmd(ctx):
